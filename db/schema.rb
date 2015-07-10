05ae08e4a182a081afdd75404f2892fbba705bf2
Anton Khaybulaev <ant.khay@gmail.com>
2015-07-11 01:58:55 +0600

    Add news and images to training

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150704231625) do
+ActiveRecord::Schema.define(version: 20150710190615) do
 
   create_table "galleries", force: :cascade do |t|
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "name"
+  end
+
+  create_table "news", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "pictures", force: :cascade do |t|
