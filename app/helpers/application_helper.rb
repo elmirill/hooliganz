905f8fcbcea2caa314857de77f2b2f08d7bea6d3
Anton Khaybulaev <ant.khay@gmail.com>
2015-07-16 03:30:04 +0600

    Add users and fix styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 	
-
+	def is_admin?
+		current_user.email == 'ant.khay@gmail.com' || current_user.email == 'hooliganz-school@mail.ru'
+	end
+	
+	
 	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, path_names: { edit: 'profile' }
   resources :news
 #	resources :galleries
 	resources :pictures
