b121d1e4692b301c2f38a1a3406438ec84454166
Anton Khaybulaev <ant.khay@gmail.com>
2015-08-05 23:24:02 +0600

    Experiment reaizing

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,7 +2,12 @@ class Picture < ActiveRecord::Base
   belongs_to :gallery
 	
 	has_attached_file :image,
-						styles: { thumb: "350x350#" },
+						styles: {
+							thumb:
+								geometry: "350x350#",
+								quality: "70",
+								format: "JPG"
+							},
 						preserve_files: true
 	validates_attachment :image, 
 						content_type: { content_type: ["image/jpeg", "image/png", "image/gif"] },
