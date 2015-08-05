47596d4c714daf0b352a25a587ca36d1aa8d0b2a
Anton Khaybulaev <ant.khay@gmail.com>
2015-08-05 23:05:31 +0600

    Fixing endpoint aws

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,1 +1,2 @@
 Paperclip::Attachment.default_options[:path] = '/hooliganz/:class/:attachment/:style/:filename'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-central-1.amazonaws.com'
