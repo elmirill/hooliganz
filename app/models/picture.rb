7a0015ddab6c11bb91cdb761c666261f476d5b2a
Anton Khaybulaev <ant.khay@gmail.com>
2015-08-05 22:34:21 +0600

    Fixing bucket var

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,9 +3,7 @@ class Picture < ActiveRecord::Base
 	
 	has_attached_file :image,
 						styles: { thumb: "350x350#" },
-						preserve_files: true,
-            :storage => :s3,
-            :bucket  => ENV['MY_BUCKET_NAME']
+						preserve_files: true
 	validates_attachment :image, 
 						content_type: { content_type: ["image/jpeg", "image/png", "image/gif"] },
 						size: { in: 0..5.megabytes }
