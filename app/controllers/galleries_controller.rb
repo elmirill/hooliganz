75456836a596922a8357b1566fa12253fb921e56
Anton Khaybulaev <ant.khay@gmail.com>
2015-08-05 11:53:47 +0600

    Hide new gallery functions

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,19 +1,19 @@
 class GalleriesController < ApplicationController
 	before_action :authenticate_user!, except: [:show]
 	
-	def new
-		@gallery = Gallery.new
-	end
-	
-	def create
-		@gallery = Gallery.new(gallery_params)
-		if @gallery.save
-			flash[:notice] = "Галерея создана"
-			redirect_to "/gallery"
-		else
-			render action: 'new'
-		end
-	end
+#	def new
+#		@gallery = Gallery.new
+#	end
+#	
+#	def create
+#		@gallery = Gallery.new(gallery_params)
+#		if @gallery.save
+#			flash[:notice] = "Галерея создана"
+#			redirect_to "/gallery"
+#		else
+#			render action: 'new'
+#		end
+#	end
 	
   def show
 		@gallery = Gallery.where(name: 'main').first
