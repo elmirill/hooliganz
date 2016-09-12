4b7a35c55ce1a7f6ff4f15038b57e831e6e163e1
Tony Khaybulaev <ant.khay@gmail.com>
2016-09-12 23:38:54 +0700

    change froala storage to s3 and change s3 bucket

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
+  include AmazonSignature
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-	
-	before_action :set_core_setting
-	
+
+	before_action :set_core_setting, :calc_amazon_hash
+
 	def set_core_setting
 		@core_setting = CoreSetting.find(1)
 	end
-	
+
+  def calc_amazon_hash
+    @amazon_hash = AmazonSignature::data_hash
+  end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,6 @@ module Hooliganz
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 		config.i18n.default_locale = :ru
+    config.autoload_paths += %W(#{config.root}/lib)
   end
 end
